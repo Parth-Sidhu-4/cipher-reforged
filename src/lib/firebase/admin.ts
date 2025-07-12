e1626447c949d61dc5a857183546fcaf3744f70b
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 00:20:26 +0530

    Hope for success

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -6,10 +6,9 @@ if (!admin.apps.length) {
 			projectId: process.env.FB_PROJECT_ID || import.meta.env?.VITE_FB_PROJECT_ID,
 
 			clientEmail: process.env.FB_CLIENT_EMAIL || import.meta.env?.VITE_FB_CLIENT_EMAIL,
-			privateKey: (process.env.FB_PRIVATE_KEY || import.meta.env?.VITE_FB_PRIVATE_KEY)?.replace(
-				/\n/g,
-				'\n'
-			)
+			privateKey: (process.env.FB_PRIVATE_KEY || import.meta.env?.VITE_FB_PRIVATE_KEY)
+				?.replace(/\\n/g, '\n')
+				.trim()
 		})
 	});
 }
