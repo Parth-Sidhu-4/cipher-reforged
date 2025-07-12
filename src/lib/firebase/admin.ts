61646a7ca7c6acfc213ec974a1f19f7635adaf39
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 00:44:58 +0530

    roo dunga me

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,12 +1,14 @@
 // src/lib/firebase/admin.ts
 import admin from 'firebase-admin';
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY || '{}');
+
 if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: process.env.FB_PROJECT_ID,
 			clientEmail: process.env.FB_CLIENT_EMAIL,
-			privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, '\n')
+			privateKey
 		})
 	});
 }
