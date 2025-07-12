cb59f214c5aff4d0aa9847f9e50266b8253a418a
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 00:54:00 +0530

    cry aa rha hai

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,14 +1,11 @@
-// src/lib/firebase/admin.ts
 import admin from 'firebase-admin';
-
-const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY || '{}');
 
 if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: process.env.FB_PROJECT_ID,
 			clientEmail: process.env.FB_CLIENT_EMAIL,
-			privateKey
+			privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n')
 		})
 	});
 }
