4021dc541554651b5ec2d8e2eb603a40dfd267a4
Parth Sidhu <mister.parth777@gmail.com>
2025-07-12 23:59:33 +0530

    Towards Deployment

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyB2Kg4YSUlQNZqsJbuX3McCuPSacJDJhZ4',
-	authDomain: 'cipher-reforged.firebaseapp.com',
-	projectId: 'cipher-reforged',
-	storageBucket: 'cipher-reforged.firebasestorage.app',
-	messagingSenderId: '215368973502',
-	appId: '1:215368973502:web:ae0c5a104c873c284e32ef'
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
