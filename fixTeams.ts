f837e28c1975088699721cba537e89b277d82bdc
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 13:22:48 +0530

    roo dunga mee

--- fixTeams.ts
+++ fixTeams.ts
@@ -8,7 +8,7 @@ if (getApps().length === 0) {
 		credential: cert({
 			projectId: process.env.FB_PROJECT_ID,
 			clientEmail: process.env.FB_CLIENT_EMAIL,
-			privateKey: process.env.FB_PRIVATE_KEY?.replace(/\n/g, '\n')
+			privateKey: process.env.FB_PRIVATE_KEY
 		})
 	});
 }
