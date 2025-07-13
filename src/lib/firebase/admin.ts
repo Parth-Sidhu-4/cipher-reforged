a09679120681377af1ce6048696e9cf62d8a4b6b
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 12:25:56 +0530

    Dukh Dard Peeda Kasht

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,27 +1,22 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-	const serviceAccount = JSON.parse(process.env.FB_SERVICE_ACCOUNT!);
+	let credentials;
+
+	if (process.env.FB_SERVICE_ACCOUNT) {
+		credentials = JSON.parse(process.env.FB_SERVICE_ACCOUNT);
+	} else {
+		credentials = {
+			projectId: process.env.FB_PROJECT_ID,
+			clientEmail: process.env.FB_CLIENT_EMAIL,
+			privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n')
+		};
+	}
 
 	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount)
+		credential: admin.credential.cert(credentials)
 	});
 }
 
 export const adminAuth = admin.auth();
 export const adminDB = admin.firestore();
-
-// import admin from 'firebase-admin';
-
-// if (!admin.apps.length) {
-// 	admin.initializeApp({
-// 		credential: admin.credential.cert({
-// 			projectId: process.env.FB_PROJECT_ID,
-// 			clientEmail: process.env.FB_CLIENT_EMAIL,
-// 			privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n')
-// 		})
-// 	});
-// }
-
-// export const adminAuth = admin.auth();
-// export const adminDB = admin.firestore();
