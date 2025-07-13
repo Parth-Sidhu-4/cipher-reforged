e5a2067ea54e976460a669e0388c91439c45fb94
Parth Sidhu <mister.parth777@gmail.com>
2025-07-13 12:23:43 +0530

    FB Tante

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,14 +1,27 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+	const serviceAccount = JSON.parse(process.env.FB_SERVICE_ACCOUNT!);
+
 	admin.initializeApp({
-		credential: admin.credential.cert({
-			projectId: process.env.FB_PROJECT_ID,
-			clientEmail: process.env.FB_CLIENT_EMAIL,
-			privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n')
-		})
+		credential: admin.credential.cert(serviceAccount)
 	});
 }
 
 export const adminAuth = admin.auth();
 export const adminDB = admin.firestore();
+
+// import admin from 'firebase-admin';
+
+// if (!admin.apps.length) {
+// 	admin.initializeApp({
+// 		credential: admin.credential.cert({
+// 			projectId: process.env.FB_PROJECT_ID,
+// 			clientEmail: process.env.FB_CLIENT_EMAIL,
+// 			privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n')
+// 		})
+// 	});
+// }
+
+// export const adminAuth = admin.auth();
+// export const adminDB = admin.firestore();
